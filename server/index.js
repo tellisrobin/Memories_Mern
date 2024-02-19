8ce52fe6f2655a964552cab75593a54345be21e9
Robin Tellis <97566347+tellisrobin@users.noreply.github.com>
2024-02-19 12:24:44 -0500

    Setup complete

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
+const CONNECTION_URL =
+  "mongodb+srv://root:root@cluster0.sdbzmgo.mongodb.net/?retryWrites=true&w=majority"; //store as ENv Variable
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {})
+  .then(() => {
+    console.log("Connected to DataBase");
+    app.listen(PORT, () => console.log(`Server running on : ${PORT}`));
+  })
+  .catch(() => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
